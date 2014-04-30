a3e1e8d63032abb3d11a4e1233ac363528716903
Ms2ger <ms2ger@gmail.com>
2014-04-30 12:48:58 +0200

    Call wrapObjectTemplate with the correct arguments.

--- src/components/script/dom/bindings/codegen/CodegenRust.py
+++ src/components/script/dom/bindings/codegen/CodegenRust.py
@@ -742,8 +742,8 @@ def getJSToNativeConversionTemplate(type, descriptorProvider, failureCode=None,
                                        onFailureNotCallable(failureCode)).define()
             template = wrapObjectTemplate(
                 template,
+                isDefinitelyObject,
                 type,
-                "nullptr",
                 failureCode)
         return (template, declType, None, needsRooting)
 
