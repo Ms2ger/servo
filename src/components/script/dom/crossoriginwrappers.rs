4328bde897636349c08d9c1b920a66da68d73a68
Ms2ger <ms2ger@gmail.com>
2014-08-26 15:35:21 +0200

    Introduce a set of proxy traps for cross-origin wrappers.

---
+++ src/components/script/dom/crossoriginwrappers.rs
@@ -0,0 +1,37 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+use js::glue::ProxyTraps;
+
+pub static proxy_handler: ProxyTraps = ProxyTraps {
+    getPropertyDescriptor: None,
+    getOwnPropertyDescriptor: None,
+    defineProperty: None,
+    getOwnPropertyNames: 0 as *const u8,
+    delete_: None,
+    enumerate: 0 as *const u8,
+
+    has: None,
+    hasOwn: None,
+    get: None,
+    set: None,
+    keys: 0 as *const u8,
+    iterate: None,
+
+    call: None,
+    construct: None,
+    nativeCall: 0 as *const u8,
+    hasInstance: None,
+    typeOf: None,
+    objectClassIs: None,
+    obj_toString: None,
+    fun_toString: None,
+    //regexp_toShared: 0 as *u8,
+    defaultValue: None,
+    iteratorNext: None,
+    finalize: None,
+    getElementIfPresent: None,
+    getPrototypeOf: None,
+    trace: None
+};
