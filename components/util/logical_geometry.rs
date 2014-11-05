df8953bfd04241266ea5bb865ae4d475c9925192
Jack Moffitt <jack@yucca.local>
2014-11-05 12:33:11 -0700

    Rust upgrade to rustc hash b03a2755193cd756583bcf5831cf4545d75ecb8a

--- components/util/logical_geometry.rs
+++ components/util/logical_geometry.rs
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-//! Geometry in flow-relative space.
+/// Geometry in flow-relative space.
 
 use geom::{Size2D, Point2D, SideOffsets2D, Rect};
 use std::cmp::{min, max};
@@ -12,10 +12,10 @@ use std::num::Zero;
 bitflags!(
     #[deriving(Encodable)]
     flags WritingMode: u8 {
-        static FlagRTL = 1 << 0,
-        static FlagVertical = 1 << 1,
-        static FlagVerticalLR = 1 << 2,
-        static FlagSidewaysLeft = 1 << 3
+        const FlagRTL = 1 << 0,
+        const FlagVertical = 1 << 1,
+        const FlagVerticalLR = 1 << 2,
+        const FlagSidewaysLeft = 1 << 3
     }
 )
 
