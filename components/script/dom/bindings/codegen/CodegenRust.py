3ee83cb1ae9b132fe996dc5eca687464a232506c
Ms2ger <ms2ger@gmail.com>
2015-01-19 10:54:25 +0100

    [BROKEN] Document.URL; assert about names

--- components/script/dom/bindings/codegen/CodegenRust.py
+++ components/script/dom/bindings/codegen/CodegenRust.py
@@ -2514,6 +2514,7 @@ class CGSpecializedMethod(CGAbstractExternMethod):
 
     @staticmethod
     def makeNativeName(descriptor, method):
+        assert re.match(r"([a-z]+)([A-Z]([a-z]+|[A-Z]*))*", method.identifier.name), method.identifier.name
         return MakeNativeName(method.identifier.name)
 
 class CGStaticMethod(CGAbstractStaticBindingMethod):
@@ -2580,6 +2581,7 @@ class CGSpecializedGetter(CGAbstractExternMethod):
 
     @staticmethod
     def makeNativeName(descriptor, attr):
+        assert re.match(r"([a-z]+)([A-Z]([a-z]+|[A-Z]*))*", attr.identifier.name), attr.identifier.name
         nativeName = MakeNativeName(attr.identifier.name)
         infallible = ('infallible' in
                       descriptor.getExtendedAttributes(attr, getter=True))
@@ -2658,6 +2660,7 @@ class CGSpecializedSetter(CGAbstractExternMethod):
 
     @staticmethod
     def makeNativeName(descriptor, attr):
+        assert re.match(r"([a-z]+)([A-Z]([a-z]+|[A-Z]*))*", attr.identifier.name), attr.identifier.name
         return "Set" + MakeNativeName(attr.identifier.name)
 
 
